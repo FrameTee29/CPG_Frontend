4499514ca00fdb241923e034668975745e59a275
FrameTee29 <teeraphat123@hotmail.com>
2020-09-13 23:25:36 +0700

    getting balance

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 const Tx = require('ethereumjs-tx').Transaction;
 const Web3 = require("web3");
-// const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/8d1234baedad4a588a49a51ac993aaf8'));
 const web3 = new Web3(Web3.givenProvider);
+// const web3 = new Web3(new Web3.providers.HttpProvider('https://kovan.infura.io/v3/8d1234baedad4a588a49a51ac993aaf8'));
 
 const StyledWrapper = styled.body`
 
 font-family: 'Poppins', sans-serif;
+
 `
 
 
@@ -17,8 +18,10 @@ const Home = () => {
   const [network, setNetwork] = useState('');
   const [userAccount, setUserAccount] = useState('');
   const [warning, setWarning] = useState('');
+  const [Balance, setBalance] = useState(0);
 
   useEffect(() => {
+    // Get current network
     if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
       window.ethereum.enable();
@@ -26,7 +29,8 @@ const Home = () => {
     startApp();
   }, [])
 
-  const startApp = () => {
+  const startApp = async () => {
+    // Get current network
     web3.eth.net.getId().then(netId => {
       console.log('netId: ' + netId)
       switch (netId) {
@@ -55,10 +59,31 @@ const Home = () => {
       }
     })
 
+    // Get Account 
     web3.eth.getAccounts().then(accounts => {
-      setUserAccount(accounts[0])
+      setUserAccount(accounts[0]);
+      reloadInfo();
     })
   }
+
+  const reloadInfo = async () => {
+    try {
+      getBalance();
+
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  const getBalance = () => {
+    console.log("COIN")
+    // web3.eth.getBalance(userAccount).then(result => {
+    //   console.log(web3.utils.fromWei(result));
+    // })
+  }
+
 
   return (
     <StyledWrapper>
@@ -75,6 +100,11 @@ const Home = () => {
           <span>Wallet </span>
           <span >Account : {userAccount}</span>
         </p>
+        <p >
+          <span>Balance: </span>
+          <span >{Balance}</span>
+        </p>
+
         <h1>FROM</h1>
         <input />
         <h1>TO</h1>
